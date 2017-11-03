0e0a88bc334dc7da12cddcdf82a0b0b303b7b844
vagarii <xil154@pitt.edu>
2017-11-03 00:12:45 -0700

    removeID

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_ID: 'client_id_aiwhN0E15uLOn28gHmpZXSdKD0qfjABcJ9xYFtVP',
-    CLIENT_SECRET: 'client_secret_Ar8vZYQaiWKF4oODmw63MH2tNB7Ecb5nL9Gpy1dx',
+    CLIENT_ID: 'xxxxxxx',
+    CLIENT_SECRET: 'xxxxxxx',
     FINGERPRINT: 'finger'
 };
